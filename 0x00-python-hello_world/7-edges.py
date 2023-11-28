5cfd5581af8216709e2cc53c38d5bd5fb11a1a86
akanSWE <a.akanSWE@gamil.com>
2023-11-28 15:34:43 +0100

    fix: 7-edges.py:4:80: E501 line too long (86 > 79 characters)

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[:3]  # include all characters starting from index n
-word_last_2 = word[-2:]  # exclude all characters from the right starting from index n
+word_first_3 = word[:3]  # include the first three characters
+word_last_2 = word[-2:]  # exclude the last two characters
 middle_word = word[1:-1]  # remove the first and last character
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
