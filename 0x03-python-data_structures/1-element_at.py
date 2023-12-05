55a8b3bbef99efc86561414a70e999857ae98e10
akanSWE <a.akanSWE@gamil.com>
2023-12-05 23:56:01 +0100

    feat: Implement the function for task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+
+    """ Retrieves an element from a list
+
+    :param my_list: The list
+    :param idx: The index
+
+    :return: The element at the index, otherwise None
+    if index is greater than length of list or index a negative number
+    """
+
+    length = len(my_list)
+
+    if length < idx < 0:
+        return None
+
+    return my_list[idx]
