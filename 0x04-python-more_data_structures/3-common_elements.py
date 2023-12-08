4b468ed248f73cffe91665399e646ac0e2d37fe1
akanSWE <a.akanSWE@gamil.com>
2023-12-08 19:01:28 +0100

    feat: Defined common_elements for task 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """ Creates a sets of common elements in two sets
+
+    :param set_1: First set
+    :param set_2: Second set
+
+    :return: Return the new set containing common elements
+
+    """
+    return set_1 & set_2  # intersection
