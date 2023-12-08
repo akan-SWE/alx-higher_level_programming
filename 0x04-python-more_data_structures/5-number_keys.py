ea03445052197bfe5084ec5e415cb618a14b204d
akanSWE <a.akanSWE@gamil.com>
2023-12-08 19:14:21 +0100

    feat: Created number_keys function for task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """ Calculates the number of keys in a dictionary
+
+    :param a_dictionary: The dictionary
+
+    :return: The number of keys in a dictionary
+    """
+    return len(list(a_dictionary.keys()))
