0e7913236523bf5a821a68afc2a9321c74368448
akanSWE <a.akanSWE@gamil.com>
2023-12-19 09:41:33 +0100

    add: Implement safe_print_integer function

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """ Prints an integer
+
+    :param value: The integer
+
+    :return: True if value is correctly printed
+    else False
+
+    """
+    try:
+        print('{:d}'.format(value))
+        return True
+    except ValueError:
+        return False
