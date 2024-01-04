ceb0b4fec8d383067b6e63189270681b78b99529
akanSWE <a.akanSWE@gamil.com>
2024-01-04 07:46:08 +0100

    feat: Added a constructor to the Square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+"""This module contains the Square class"""
+
+
+class Square:
+    """Defines a square
+
+    Attribute:
+        size: size of the square
+    Methods:
+        __init__: a constructor that initializes self
+
+    """
+    def __init__(self, size=0):
+        self.__size = size
