5f3a107f02ae21016ed8af6730263822ef334acc
akanSWE <a.akanSWE@gamil.com>
2024-01-08 10:50:13 +0100

    add: Handle logical operators on the Square class

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+
+"""This module contains the Square class"""
+
+
+class Square:
+    """Defines a square class
+
+    Attribute:
+        size: size of the square
+    Methods:
+        __init__: initialize size and also verify the data
+        area: returns the area of a square
+        gt, lt, ge, le, eq, ne: use to compare the size between squares
+    Property:
+        size (getter): Get the current size of the square
+        size (setter): Set the current size of the square
+
+
+    """
+    def __init__(self, size=0):
+        self.size = size  # call the (setter) size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def __eq__(self, other):
+        if self.size == other.size:
+            return True
+        return False
+
+    def __le__(self, other):
+        if self.size <= other.size:
+            return True
+        return False
+
+    def __ge__(self, other):
+        if self.size >= other.size:
+            return True
+        return False
+
+    def __ne__(self, other):
+        if self.size != other.size:
+            return True
+        return False
+
+    def __gt__(self, other):
+        if self.size > other.size:
+            return True
+        return False
+
+    def __lt__(self, other):
+        if self.size < other.size:
+            return True
+        return False
