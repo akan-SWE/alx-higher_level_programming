3f9c75152eae6828f85b6740b06c4569adbb3ed5
akanSWE <a.akanSWE@gamil.com>
2024-01-30 17:11:11 +0100

    feat: Define the area and perimeter methods

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+"""Defines a Rectangle class"""
+
+
+class Rectangle:
+    """Defines a Rectangle class
+
+    Attribute:
+        height (int): height of the Rectangle
+        width (int): width of the Rectangle
+
+    Properties:
+        height (setter and getter): set and get the height of the Rectangle
+        width (setter and getter): set and get the width of the Rectangle
+
+    Methods:
+        area: area of the Rectangle (h * w)
+        perimeter: perimeter of the Rectangle 2(h + w)
+    """
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        # if width or height is equal to 0, perimeter is equal to 0
+        return 0 if not self.width or not self.height \
+            else 2 * (self.height + self.width)
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+
+        self.__height = value
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+
+        self.__width = value
