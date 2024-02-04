53ff34f51eed5d38800a6e45f7e1490502a2be9e
akanSWE <a.akanSWE@gamil.com>
2024-02-04 07:06:35 +0100

    fix: pycodestyle errors

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-
 """Defines the LockedClass
 
 Example:
     >>> lc = LockedClass()
-    
-    No error occurred when first_name is added dynamically
+
+    No Error occurred when first_name is added dynamically
     >>> lc.first_name = "Snow"
 
-    error occurred with other attribute names
+    Error occurred with other attribute names
     >>> lc.last_name = "White"
     Traceback (most recent call last):
     ...
     AttributeError: 'LockedClass' object has no attribute 'last_name'
 
 """
+
+
 class LockedClass:
-    """A class that does not have class or object attribute and can't be added dynamically
-    except first_name
+    """A class that does not have class or object attribute
+    and can't be added dynamically except first_name
 
     Attribute:
         __slots__: The list contain only first_name
