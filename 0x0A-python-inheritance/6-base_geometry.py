9b37cbc6654f26afe1448b30fce910848ff26616
akanSWE <a.akanSWE@gamil.com>
2024-02-06 15:30:00 +0100

    feat: implement the add method to the BaseGeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define BaseGeometry (based on 5-base_geometry.py)"""
+
+
+class BaseGeometry:
+    """Defines a geometry class
+
+    Methods:
+        area: currently raises an exception
+    """
+
+    def area(self):
+        raise Exception('area() is not implemented')
