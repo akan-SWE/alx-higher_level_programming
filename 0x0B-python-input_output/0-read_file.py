3b73760f2cb2366c5fd02036c48baff0580a6f01
akanSWE <a.akanSWE@gamil.com>
2024-02-08 17:49:14 +0100

    add: define read_file function

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Defines the read_file function"""
+
+
+def read_file(filename=""):
+    """ Reads text from a file
+
+    param:
+        filename: name of the file
+    """
+    with open(filename, encoding='UTF-8') as fp:
+        print(fp.read().strip('\n'))
