90ae3de422ae9f9f8d471ce70f1e600b049a6e94
akanSWE <a.akanSWE@gamil.com>
2024-02-13 14:55:56 +0100

    add: test cases to unit test of the base class

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+
+    def test_id_increment_when_none(self):
+        Base._Base__nb_objects = 0  # reset __nb_objects to zero for each test
+        b = Base()
+        self.assertEqual(b.id, 1)
+
+    def test_id_when_not_none(self):
+        Base._Base__nb_objects = 0  # reset __nb_objects to zero for each test
+        b = Base(32)
+        self.assertEqual(b.id, 32)
+
+    def test_id_with_multiple_instance(self):
+        Base._Base__nb_objects = 0  # reset __nb_objects to zero for each test
+        b1 = Base()
+        b2 = Base()
+        b3 = Base(32)
+        b4 = Base(42)
+        b5 = Base()
+        self.assertEqual(b5.id, 3)
