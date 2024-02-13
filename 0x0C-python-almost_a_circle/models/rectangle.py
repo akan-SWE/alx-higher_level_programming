ef82afa2abfa54eada5e3453a07d62ee16b6d0d0
akanSWE <a.akanSWE@gamil.com>
2024-02-13 18:33:13 +0100

    add logic that verifies inputs before assigning to attribute

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -42,15 +42,7 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, height):
-        self.__height = height
+        super().__init__(id)  # iniatilize it base
 
     @property
     def width(self):
@@ -58,7 +50,29 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, width):
+        """Verifies width before assigning
+        It must be an integr and greater than 0
+        """
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+        elif width < 1:
+            raise ValueError('width must be > 0')
         self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """Verifies height before assigning
+        It must be an integer and greater than 0
+        """
+        if not isinstance(height, int):
+            raise TypeError('height must be an integer')
+        elif height < 1:
+            raise ValueError('height must be > 0')
+        self.__height = height
 
     @property
     def x(self):
@@ -66,6 +80,13 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, x):
+        """Verifies x before assigning
+        It must be an integer and greater or equal to 0
+        """
+        if not isinstance(x, int):
+            raise TypeError('x must be an integer')
+        elif x < 0:
+            raise ValueError('x must be >= 0')
         self.__x = x
 
     @property
@@ -74,4 +95,11 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, y):
+        """Verifies y before assigning
+        It must an integer and greater than or equal to 0
+        """
+        if not isinstance(y, int):
+            raise TypeError('y must be an integer')
+        elif y < 0:
+            raise ValueError('y must be >= 0')
         self.__y = y
