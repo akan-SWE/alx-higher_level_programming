2ddce3e2ce93116862dbd2fac61b681c97cb4011
akanSWE <a.akanSWE@gamil.com>
2024-02-13 18:19:21 +0100

    update: add a setUp method to rest __nb_objects for each test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,15 +5,16 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
 
+    def setUp(self):
+        Base._Base__nb_objects = 0  # reset __nb_objects to zero for each test
+
     def test_id_increment_when_none(self):
         b = Base()
         self.assertEqual(b.id, 1)
-        Base._Base__nb_objects = 0  # reset __nb_objects to zero for each test
 
     def test_id_when_not_none(self):
         b = Base(32)
         self.assertEqual(b.id, 32)
-        Base._Base__nb_objects = 0  # reset __nb_objects to zero for each test
 
     def test_id_with_multiple_instance(self):
         b1 = Base()
@@ -22,4 +23,3 @@ class TestBase(unittest.TestCase):
         b4 = Base(42)
         b5 = Base()
         self.assertEqual(b5.id, 3)
-        Base._Base__nb_objects = 0  # reset __nb_objects to zero for each test
