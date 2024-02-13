7696c40e64c0fa073ffee1dc17fb1c8e5366e441
akanSWE <a.akanSWE@gamil.com>
2024-02-13 16:45:00 +0100

    add: correct placement to reset the __nb_objects attribute

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,20 +6,20 @@ from models.base import Base
 class TestBase(unittest.TestCase):
 
     def test_id_increment_when_none(self):
-        Base._Base__nb_objects = 0  # reset __nb_objects to zero for each test
         b = Base()
         self.assertEqual(b.id, 1)
+        Base._Base__nb_objects = 0  # reset __nb_objects to zero for each test
 
     def test_id_when_not_none(self):
-        Base._Base__nb_objects = 0  # reset __nb_objects to zero for each test
         b = Base(32)
         self.assertEqual(b.id, 32)
+        Base._Base__nb_objects = 0  # reset __nb_objects to zero for each test
 
     def test_id_with_multiple_instance(self):
-        Base._Base__nb_objects = 0  # reset __nb_objects to zero for each test
         b1 = Base()
         b2 = Base()
         b3 = Base(32)
         b4 = Base(42)
         b5 = Base()
         self.assertEqual(b5.id, 3)
+        Base._Base__nb_objects = 0  # reset __nb_objects to zero for each test
