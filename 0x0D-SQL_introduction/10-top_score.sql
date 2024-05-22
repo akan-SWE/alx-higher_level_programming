8d0b5e31acb4698df908f5d0df6a0c1fe3eaca22
akan-SWE <a.akanSWE@gmail.com>
2024-05-22 04:00:12 -0700

    implement task 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- Lists all records of the table 'second table` of the database 'hbtn_0c_0'
+-- in the MySQL server.
+-- Record displays both score and name
+-- Result is ordered by score (top first)
+SELECT score, name FROM `second_table`
+ORDER BY score DESC;
+
