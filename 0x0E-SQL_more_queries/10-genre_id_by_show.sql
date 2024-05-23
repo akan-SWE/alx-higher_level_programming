83bd2ba4457d789789b7a930cff0835ced73f437
akanSWE <a.akanSWE@gamil.com>
2024-05-23 21:13:58 +0100

    remove redundancy on task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,8 +5,7 @@
 -- Results is sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 
 
-SELECT ts.title, tsg.genre_id FROM tv_shows ts
+SELECT ts.title, tsg.genre_id
+FROM tv_shows ts
 JOIN tv_show_genres tsg ON ts.id = tsg.show_id
-JOIN tv_genres tg ON tsg.genre_id = tg.id
 ORDER BY ts.title, tsg.genre_id;
-
