115b96eb7b094863ea12cc4f33f314d46d05c907
akanSWE <a.akanSWE@gamil.com>
2024-06-21 01:23:59 +0100

    feat: Implement task 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+/**
+ * Rectangle - Defines a rectangle
+ */
+class Rectangle {
+  width;
+  height;
+
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
