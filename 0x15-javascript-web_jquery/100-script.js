df36431a990116e072dd46c35ae5db2caca59745
akanSWE <a.akanSWE@gamil.com>
2024-08-08 05:00:16 +0100

    Updates the text color of the `<header>` element to red.

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/**
+ * Updates the text color of the `<header>` element to red.
+ */
+window.onload = function () {
+  document.querySelector('header').style.color = '#FF0000';
+};
