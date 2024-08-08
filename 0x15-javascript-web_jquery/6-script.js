07a1e97744e3865a9ee9a06c3f5c4b8c31b97925
akanSWE <a.akanSWE@gamil.com>
2024-08-08 04:04:19 +0100

    Add script that adds a text to the `<header>` element

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,8 @@
+/* global $ */
+/**
+ * Updates a text of the `<header>` element to `New Header!!!` when the
+ * user clicks.
+ */
+$('DIV#update_header').click(function () {
+  $('header').html('New Header!!!');
+});
