79e1c668edbd742b063ed0379ae83c8ba21d26cf
akanSWE <a.akanSWE@gamil.com>
2024-08-12 14:19:35 +0100

    Add script to print "Hello" based on language, triggered by Enter key or button click

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,26 @@
+/* global $ */
+
+$(function () {
+  // Event listener for button click
+  const url = 'https://hellosalut.stefanbohacek.dev/?lang=';
+  $('INPUT#btn_translate').click(function () {
+    const lang = $('INPUT#language_code').val(); // Get the value inside the event handler
+    $.get(url + lang, function (data, status) {
+      if (status === 'success') {
+        $('DIV#hello').text(data.hello);
+      }
+    });
+  });
+
+  // Event listener for Enter key press
+  $('INPUT#language_code').keypress(function (event) {
+    if (event.which === 13) {
+      const lang = $('INPUT#language_code').val(); // Get the value inside the event handler
+      $.get(url + lang, function (data, status) {
+        if (status === 'success') {
+          $('DIV#hello').text(data.hello);
+        }
+      });
+    }
+  });
+});
